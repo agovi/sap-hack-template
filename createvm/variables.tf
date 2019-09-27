de2f959abb62c747be3a0603354869d1171c5c28
Karthik Venkatraman <44262238+karthikvenkat17@users.noreply.github.com>
2019-09-27 15:15:31 +0100

    Create variables.tf

---
+++ createvm/variables.tf
@@ -0,0 +1,42 @@
+variable "rgname" {
+    description = "Name of the resource group to deploy the resources"
+ }
+variable "location" {
+    description = "Specifies the location to deploy the resources"
+    default = "WestEurope"
+}
+variable "vmname" {
+  description = "Namw of the VM to be created"  
+}
+
+variable "adminuser" {
+  description = "Username for logging in to the Virtual Machines"
+  default = "azureuser"
+}
+variable "adminpassword" {
+  description = "Password for logging in to the Virtual Machines"
+}
+variable "tags" {
+  description = "A map of tags to the deployed resources. Empty by default."
+  type        = "map"
+  default     = {}
+}
+variable "image_id" {
+    description = "Custom image id"
+  }
+variable "private_ip" {
+  description = "Private IP address"
+ }
+variable "subnet_id" {
+  description = "Subnet ID for creating the VM"
+  
+}
+variable "vmtype" {
+  description = "VM type to be deployed"
+}
+variable "vmsize" {
+  description = "VM size to be deployed"
+  
+}
+
+
