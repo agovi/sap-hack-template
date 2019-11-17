1bb0a3a3166c832a84e7bc6efdfc27e4d4ab53da
Karthik Venkatraman <karthik.venkatraman@microsoft.com>
2019-11-17 22:15:58 +0000

    image version

--- locals.tf
+++ locals.tf
@@ -8,7 +8,8 @@ locals {
   }
   nfsvm0 = {
     vmname = "TST-nfs-vm-0"
-    vmimage = "/subscriptions/afbba066-2190-4c21-b9ec-4a945b7bfbcc/resourceGroups/sap-cluster-images/providers/Microsoft.Compute/galleries/s4sleshana/images/slesnfsvm0/versions/1.0.0"
+    vmimage = "/subscriptions/afbba066-2190-4c21-b9ec-4a945b7bfbcc/resourceGroups/sap-cluster-images/providers/Microsoft.Compute/galleries/s4sleshana/images/slesnfsvm0/versions/2.0.0"
+    #vmimage = "/subscriptions/afbba066-2190-4c21-b9ec-4a945b7bfbcc/resourceGroups/sap-cluster-images/providers/Microsoft.Compute/galleries/s4sleshana/images/slesnfsvm0/versions/1.0.0"
     #vmimage = "/subscriptions/afbba066-2190-4c21-b9ec-4a945b7bfbcc/resourceGroups/sap-cluster-images/providers/Microsoft.Compute/images/TST-nfs-vm-0-image"
     vmpip = "172.16.3.12"
     vmsize = "Standard_D2s_v3"
@@ -17,14 +18,16 @@ locals {
   
   nfsvm1 = {
     vmname = "TST-nfs-vm-1"
-    vmimage = "/subscriptions/afbba066-2190-4c21-b9ec-4a945b7bfbcc/resourceGroups/sap-cluster-images/providers/Microsoft.Compute/galleries/s4sleshana/images/slesnfsvm1/versions/1.0.0"
+    vmimage = "/subscriptions/afbba066-2190-4c21-b9ec-4a945b7bfbcc/resourceGroups/sap-cluster-images/providers/Microsoft.Compute/galleries/s4sleshana/images/slesnfsvm1/versions/2.0.0"
+    #vmimage = "/subscriptions/afbba066-2190-4c21-b9ec-4a945b7bfbcc/resourceGroups/sap-cluster-images/providers/Microsoft.Compute/galleries/s4sleshana/images/slesnfsvm1/versions/1.0.0"
     #vmimage = "/subscriptions/afbba066-2190-4c21-b9ec-4a945b7bfbcc/resourceGroups/sap-cluster-images/providers/Microsoft.Compute/images/TST-nfs-vm-1-image"
     vmpip = "172.16.3.11"
     vmsize = "Standard_D2s_v3"
   }  
  xscsvm0 = {
     vmname = "TST-xscs-vm-0"
-    vmimage = "/subscriptions/afbba066-2190-4c21-b9ec-4a945b7bfbcc/resourceGroups/sap-cluster-images/providers/Microsoft.Compute/galleries/s4sleshana/images/slesxscs0/versions/1.0.0"
+    vmimage = "/subscriptions/afbba066-2190-4c21-b9ec-4a945b7bfbcc/resourceGroups/sap-cluster-images/providers/Microsoft.Compute/galleries/s4sleshana/images/slesxscs0/versions/2.0.0"
+    #vmimage = "/subscriptions/afbba066-2190-4c21-b9ec-4a945b7bfbcc/resourceGroups/sap-cluster-images/providers/Microsoft.Compute/galleries/s4sleshana/images/slesxscs0/versions/1.0.0"
     #vmimage = "/subscriptions/afbba066-2190-4c21-b9ec-4a945b7bfbcc/resourceGroups/sap-cluster-images/providers/Microsoft.Compute/images/TST-xscs-vm-0-image"
     vmpip = "172.16.3.15"
     vmsize = "Standard_D2s_v3"
@@ -32,7 +35,8 @@ locals {
   }  
  xscsvm1 = {
     vmname = "TST-xscs-vm-1"
-    vmimage = "/subscriptions/afbba066-2190-4c21-b9ec-4a945b7bfbcc/resourceGroups/sap-cluster-images/providers/Microsoft.Compute/galleries/s4sleshana/images/slesxscs1/versions/1.0.0"
+    vmimage = "/subscriptions/afbba066-2190-4c21-b9ec-4a945b7bfbcc/resourceGroups/sap-cluster-images/providers/Microsoft.Compute/galleries/s4sleshana/images/slesxscs1/versions/2.0.0"
+    #vmimage = "/subscriptions/afbba066-2190-4c21-b9ec-4a945b7bfbcc/resourceGroups/sap-cluster-images/providers/Microsoft.Compute/galleries/s4sleshana/images/slesxscs1/versions/1.0.0"
     #vmimage = "/subscriptions/afbba066-2190-4c21-b9ec-4a945b7bfbcc/resourceGroups/sap-cluster-images/providers/Microsoft.Compute/images/TST-xscs-vm-1-image"
     vmpip = "172.16.3.14"
     vmsize = "Standard_D2s_v3"
